bf70962ef5fdd9293b52eebb5dc4608f797fad6d
ngocnb <ngocnb@ttc-solutions.com>
2020-09-16 14:12:01 +0700

    test commit

---
+++ WhoAreYou/undoCommitnotpushedtoworkingtree.swift
@@ -0,0 +1,36 @@
+//
+//  undoCommitnotpushedtoworkingtree.swift
+//  WhoAreYou
+//
+//  Created by NgocNB on 9/16/20.
+//  Copyright © 2020 M'haimdat omar. All rights reserved.
+//
+
+import Foundation
+import  UIKit
+
+class undoCommitnotpushedtoworkingtree: UIViewController , UITableViewDelegate, UITableViewDataSource{
+
+    let tablview = UITableView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height), style: .plain)
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.addSubview(tablview)
+        tablview.delegate = self
+        tablview.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 110
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = "Demo label"
+        return cell
+    }
+
+}
